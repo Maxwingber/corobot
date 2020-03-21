d6f3a7328ecda3b5507dca3cbe716db604e3f777
Maximilian Wingbermühle <max@texwork.com>
2020-03-21 10:00:21 +0100

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_21_083830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "answers", force: :cascade do |t|
+    t.string "text"
+    t.integer "link_question"
+    t.bigint "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "answers", "questions"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Answer.destroy_all
+Question.destroy_all
+
+puts 'Starts Seeding...'
+# __________________________________________________________________________User
+puts '   Creating Questions'
+
+Question.create!({
+  name: "eins",
+  text: 'Hier ist die erste Frage'
+})
+
+puts '   Creating Answers'
+
+Answer.create!({
+  question: Question.find_by(name: "eins"),
+  link_question: 1,
+  text: 'Hier ist die erste Frage'
+})
